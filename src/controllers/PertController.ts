2494f4cfa95f716e7665de78db87644629b7723c
Rodrigo322 <rodrigollrdglucas@gmail.com>
2023-04-17 10:10:10 -0300

    crud pert

--- src/controllers/PertController.ts
+++ src/controllers/PertController.ts
@@ -7,6 +7,7 @@ export class PertController {
   async create(request: Request, response: Response) {
     const { title, description, optimistic, nominal, pessimistic, unitTime } =
       request.body;
+
     const { userId } = request.params;
 
     const estimated = calculate_pert(optimistic, nominal, pessimistic);
@@ -29,5 +30,52 @@ export class PertController {
     });
 
     return response.status(201).json(pert);
+  }
+
+  async getPerts(request: Request, response: Response) {
+    const perts = await prisma.pert.findMany();
+
+    return response.status(200).json(perts);
+  }
+
+  async getPertsByUser(request: Request, response: Response) {
+    const { userId } = request.params;
+
+    const perts = await prisma.pert.findMany({
+      where: { User: { id: userId } },
+    });
+
+    return response.status(200).json(perts);
+  }
+
+  async getPert(request: Request, response: Response) {
+    const { id } = request.params;
+    const pert = await prisma.pert.findUnique({
+      where: { id },
+    });
+    return response.status(200).json(pert);
+  }
+
+  async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { title, description, optimistic, nominal, pessimistic, unitTime } =
+      request.body;
+
+    const estimated = calculate_pert(optimistic, nominal, pessimistic);
+
+    const pert = await prisma.pert.update({
+      where: { id },
+      data: {
+        title,
+        description,
+        optimistic,
+        nominal,
+        pessimistic,
+        unitTime,
+        estimated: Number(estimated.toFixed(2)),
+      },
+    });
+
+    return response.status(200).json(pert);
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -13,3 +13,7 @@ router.get("/get-user", userController.getUser);
 router.get("/get-users", userController.getUsers);
 
 router.post("/create-pert/:userId", pertController.create);
+router.get("/get-pert/:id", pertController.getPert);
+router.get("/get-perts-by-user/:userId", pertController.getPertsByUser);
+router.get("/get-perts", pertController.getPerts);
+router.put("/put-pert/:id", pertController.update);
